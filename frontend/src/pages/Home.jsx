fac39f5c4fd3e18cc18bd98a2c1a466164ba5aaf
Louis Benedict Archie <louisbenedict98@gmail.com>
2024-06-05 11:18:52 +0700

    fix: rename home.jsx to welcome.jsx. redirect user to homepage after log in

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,29 @@
-import { Navbar } from "../components/Navbar";
+import React from 'react';
+import { Navbar}  from '../components/Navbar';
 import { Link } from "react-router-dom";
 
 export function Home() {
-  return (
-    <div>
-      <Navbar />
-      <div className="flex flex-col items-center mt-6 lg:mt-20">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center font-bold tracking-wide">
-          Plan and organize together
-        </h1>
-        <p className="mt-10 text-lg text-center text-black max-w-4xl">
-          Simplify task management for your team. Collaborate effortlessly,
-          assign tasks, and track progress together with SecondBrain.
-        </p>
-        <div className="flex justify-center my-10">
-          <Link
-            to="/register"
-            href="#"
-            className="bg-black text-white py-3 px-4 mx-3 rounded-md"
-          >
-            Start for free
-          </Link>
+    return (
+        <div>
+          <Navbar />
+          <div className="flex flex-col items-center mt-6 lg:mt-28  ">
+            <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center font-bold tracking-wide">
+              Plan and organize together
+            </h1>
+            <p className="mt-10 text-lg text-center text-black max-w-4xl">
+              Start your journey with SecondBrain by creating a team and inviting your team members.
+            </p>
+            <div className="flex justify-center my-10">
+              <Link
+                to="/create_team"
+                href="#"
+                className="bg-black text-white py-3 px-4 mx-3 rounded-md"
+              >
+                Create a team
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-}
+      );
+};
+
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,55 +1,72 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import loginImg from '../assets/login.jpg';
-import { login } from '../actions/Account.action';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import loginImg from "../assets/login.jpg";
+import { login } from "../actions/Account.action";
 
 export function Login() {
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const handleLogin = async (e) => {
         e.preventDefault();
         const response = await login(email, password);
-        if (email === '' || password === '') {
-            setError('Email and password must be filled');
+        if (email === "" || password === "") {
+            setError("Email and password must be filled");
             return;
         }
         if (response.success) {
-            navigate('/teams');
+            navigate("/home");
         } else {
             setError(response.data.message);
         }
     };
 
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-            <div className='hidden sm:block'>
-                <img className='w-full h-full object-cover' src={loginImg} alt="" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+            <div className="hidden sm:block">
+                <img className="w-full h-full object-cover" src={loginImg} alt="" />
             </div>
 
-            <div className='bg-gray-100 flex flex-col justify-center'>
-                <form className='max-w-[400px] w-full mx-auto bg-white p-4' onSubmit={handleLogin}>
-                    <h2 className='text-4xl font-bold text-center py-6'>Welcome!</h2>
-                    <div className='flex flex-col py-2'>
+            <div className="bg-gray-100 flex flex-col justify-center">
+                <form
+                    className="max-w-[400px] w-full mx-auto bg-white p-4 rounded-lg shadow-md"
+                    onSubmit={handleLogin}
+                >
+                    <h2 className="text-4xl font-bold text-center py-6">Welcome!</h2>
+                    <div className="flex flex-col py-2">
                         <label>Email</label>
-                        <input className='border p-2' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input
+                            className="border p-2 rounded-lg"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
-                    <div className='flex flex-col py-2'>
+                    <div className="flex flex-col py-2">
                         <label>Password</label>
-                        <input className='border p-2' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input
+                            className="border p-2 rounded-lg"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
-                    <button className='border w-full my-5 py-2 bg-black hover:bg-gray-600 text-white'>Log in</button>
-                    <div className='text-center mt-2 mb-4'>
-                        <p>Don't have an account? <Link to="/register"><strong>Register now</strong></Link></p>
+                    <button className="border w-full my-5 py-2 bg-black hover:bg-gray-600 text-white rounded-lg">
+                        Log in
+                    </button>
+                    <div className="text-center mt-2 mb-4">
+                        <p>
+                            Don't have an account?{" "}
+                            <Link to="/register">
+                                <strong>Register now</strong>
+                            </Link>
+                        </p>
                     </div>
-                    {error && <div className='text-red-500 text-center'>{error}</div>}
+                    {error && <div className="text-red-500 text-center">{error}</div>}
                 </form>
             </div>
         </div>
-    )
+    );
 }
-
-
-
